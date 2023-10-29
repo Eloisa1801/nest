05395ee580119eedc0a78814440db67d81165e7e
Eloisa1801 <99847182+Eloisa1801@users.noreply.github.com>
2023-10-29 14:55:41 -0400

    Update app.module.ts //swagger

--- nest-lprog-2023-master/src/app.module.ts
+++ nest-lprog-2023-master/src/app.module.ts
@@ -5,10 +5,25 @@ import { PrismaService } from './database/prisma.service';
 import { UserService } from './service/user.service';
 import { UserModule } from './model/user/user.module';
 import { PrismaModule } from './database/prisma.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+
+//config swagger
+//npm install --save @nestjs/swagger swagger-ui-express
 
 @Module({
   imports: [UserModule, PrismaModule],
   controllers: [AppController],
   providers: [AppService, PrismaService, UserService],
 })
-export class AppModule {}
+export class AppModule {
+  constructor() {
+    const config = new DocumentBuilder()
+      .setTitle('API Documentation')
+      .setDescription('Documentação da API')
+      .setVersion('1.0.0')
+      .build();
+
+    const document = SwaggerModule.createDocument(this.app, config);
+    SwaggerModule.setup('api', this.app, document);
+  }
+}
